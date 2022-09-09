e7e44d80cccaca408f47d71017c85d89514dfcbe
oluniyi <olajideoluniyi@gmail.com>
2022-09-08 19:07:42 -0700

    prints the sizes of different variable sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,8 +10,8 @@ int main(void)
 
 printf("Size of a char: %c byte(s)", sizeof(char));
 printf("Size of an int: %d byte(s)", sizeof(int)
-printf("Size of a char: %ld byte(s)", sizeof(long int));
-printf("Size of a char: %lld byte(s)", sizeof(long long int));
+printf("Size of a long int: %ld byte(s)", sizeof(long int));
+printf("Size of a long long int: %lld byte(s)", sizeof(long long int));
 printf("Size of a float: %f byte(s)", sizeof(float));
 
 return (0);
