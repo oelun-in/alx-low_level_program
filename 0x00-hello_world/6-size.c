cc04d32effa694404e4769c7447347add4f51715
oluniyi <olajideoluniyi@gmail.com>
2022-09-08 19:03:41 -0700

    prints the size of variable types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) success
+ *
+ */
+int main(void)
+{
+
+printf("Size of a char: %c byte(s)", sizeof(char));
+printf("Size of an int: %d byte(s)", sizeof(int)
+printf("Size of a char: %ld byte(s)", sizeof(long int));
+printf("Size of a char: %lld byte(s)", sizeof(long long int));
+printf("Size of a float: %f byte(s)", sizeof(float));
+
+return (0);
+
+}
