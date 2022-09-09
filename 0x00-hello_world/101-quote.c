c715e3c8b29e7b15d3720306aeaa3ea6f5e24e1f
oluniyi <olajideoluniyi@gmail.com>
2022-09-08 23:30:47 -0700

    prints out a quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
+ *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, str1, 59);
+char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, quote, size);
 return (1);
 }
