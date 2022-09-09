a3a9e5568910b95bcb5c53af73bf7f8ceadf5649
oluniyi <olajideoluniyi@gmail.com>
2022-09-08 20:58:42 -0700

    quote by Dora Korpar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: Always a success
+ *
+ *
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n");
+
+return (0);
+}
