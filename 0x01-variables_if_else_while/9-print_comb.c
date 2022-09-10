0d3992addb5b66771ee1f79a487bcae6bd6670eb
oluniyi <olajideoluniyi@gmail.com>
2022-09-09 19:32:22 -0700

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description: prints combination
+ * Return: 0
+ */
+int main(void)
+{
+int c = 0;
+while (c < 10)
+{
+putchar(48 + c);
+if (c != 9)
+{
+putchar(',');
+putchar(' ');
+}
+c++;
+}
+putchar('\n');
+return (0);
+}
