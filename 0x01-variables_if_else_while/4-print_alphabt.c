b604d68b11f580d54ef2e6a8e3d8cfd120e12229
oluniyi <olajideoluniyi@gmail.com>
2022-09-09 18:52:14 -0700

    prints all letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints alphabet in lower case followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+}
+c++;
+}
+putchar('/n');
+return (0);
+}
