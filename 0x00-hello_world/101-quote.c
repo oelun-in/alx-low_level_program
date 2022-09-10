e07f169282e1c94c803e31953e236eea56c851d2
oluniyi <olajideoluniyi@gmail.com>
2022-09-09 20:56:42 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, quote, size);
+write(2, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
 return (1);
 }
