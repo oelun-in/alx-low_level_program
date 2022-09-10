6f5089567860a42207878b183a194b998586c89a
oluniyi <olajideoluniyi@gmail.com>
2022-09-09 19:16:50 -0700

    prints reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description: prints in reverse
+ * Return: 0
+ */
+int main(void)
+{
+char c = 'z';
+while (c >= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
