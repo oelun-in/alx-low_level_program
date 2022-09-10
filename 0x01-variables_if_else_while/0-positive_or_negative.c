77b583471bac9d3677125ec46238c5046d35c745
oluniyi <olajideoluniyi@gmail.com>
2022-09-09 17:48:54 -0700

    changes made

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main block
+ * Description: prints a random number and tell if it is positive or not
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+srand(time(0);
+n = rand() - RAND_MAX / 2;
+/*your code goes here*/
+if (n > 0);
+/*your code goes here*/
+printf("%d is positive\n", n);
+/*your code goes here*/
+else if (n < 0)
+/*your code goes here*/
+printf("%d is negative\n", n);
+/*your code goes here*/
+else
+/*your code goes here*/
+printf("%d is zero\n", n);
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+this repositorycontains a list of c programs and scripts on conditional statements.
