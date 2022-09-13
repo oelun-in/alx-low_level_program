d777b665537682ec6ffac9f9f425b7cc00d03f36
oluniyi <olajideoluniyi@gmail.com>
2022-09-13 16:24:15 -0700

    creates a function and print lower case alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: Always success
+ */
+int main(void)
+{
+void print_alphabet(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+_putchar('ch');
+ch++;
+}
+_putchar('\n');
+}
+print_alphabet();
+return (0);
+}
